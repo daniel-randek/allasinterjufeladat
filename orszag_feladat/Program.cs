1ed2b4a196758ca427e76456f77fb85ed5b855b6
Daniel-Randek <radani94@gmail.com>
2020-06-27 10:52:06 +0200

    Létrehozva az objektummodell, a program és a postgres kapcsolat kezdete.

--- orszag_feladat/Program.cs
+++ orszag_feladat/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
 
 namespace orszag_feladat
 {
@@ -6,7 +8,23 @@ namespace orszag_feladat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var url = "https://restcountries.eu/rest/v2/all";
+            var countries = _download_serialized_json_data<Country>(url);
+            Console.WriteLine(countries);
+        }
+
+        private static T _download_serialized_json_data<T>(string url) where T : new()
+        {
+            using (var w = new WebClient())
+            {
+                var json_data = string.Empty;
+                try
+                {
+                    json_data = w.DownloadString(url);
+                }
+                catch (Exception) { }
+                return !string.IsNullOrEmpty(json_data) ? JsonConvert.DeserializeObject<T>(json_data) : new T();
+            }
         }
     }
 }
---
+++ orszag_feladat/postgres.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using MiNET;
+using MiNET.UI;
+using Npgsql;
+
+namespace orszag_feladat
+{
+    public partial class PostGres : Form
+    {
+        private DataSet ds = new DataSet();
+        private DataTable dt = new DataTable();
+        public PostGres()
+        {
+        }
+
+        public override void FromJson(string json, Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
