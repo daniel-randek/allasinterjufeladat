193108b457f93aa5e294bfcc193bb51e4084755c
Daniel-Randek <radani94@gmail.com>
2020-06-27 06:46:11 +0200

    Add project files.

---
+++ orszag_feladat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace orszag_feladat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
